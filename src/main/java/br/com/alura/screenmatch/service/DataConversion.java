0f3a5b5ae3b411f2be453eaa8f589494ccedf9d2
valdez <leonardovaldez.sn@gmail.com>
2026-03-03 23:30:54 -0300

    Add DataConversion class

---
+++ src/main/java/br/com/alura/screenmatch/service/DataConversion.java
@@ -0,0 +1,9 @@
+package br.com.alura.screenmatch.service;
+
+import br.com.alura.screenmatch.model.TvShowDTO;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class DataConversion {
+    private ObjectMapper mapper = new ObjectMapper();
+
+}
