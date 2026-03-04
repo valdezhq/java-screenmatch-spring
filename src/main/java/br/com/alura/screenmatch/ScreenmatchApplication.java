c70fb4fa82f90a76e76bf20229d597e4af4f0849
valdez <leonardovaldez.sn@gmail.com>
2026-03-03 23:27:51 -0300

    Implement CommandLineRunner on Main

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,13 +1,21 @@
 package br.com.alura.screenmatch;
 
+import br.com.alura.screenmatch.service.APIConsumption;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ScreenmatchApplication  {
+public class ScreenmatchApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScreenmatchApplication.class, args);
 	}
 
+    @Override
+    public void run(String... args) throws Exception {
+        var apiConsumption = new APIConsumption();
+        var json = apiConsumption.obtainData("");
+        System.out.println(json);
+    }
 }
