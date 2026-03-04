cdbd4f13e914a4b68e832d31d2d1ff827b9e8f94
valdez <leonardovaldez.sn@gmail.com>
2026-03-03 23:58:58 -0300

    Implement the IDataConversion interface and create a generic instance reading the Json value

--- src/main/java/br/com/alura/screenmatch/service/DataConversion.java
+++ src/main/java/br/com/alura/screenmatch/service/DataConversion.java
@@ -1,9 +1,17 @@
 package br.com.alura.screenmatch.service;
 
-import br.com.alura.screenmatch.model.TvShowDTO;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-public class DataConversion {
+public class DataConversion implements IDataConversion{
     private ObjectMapper mapper = new ObjectMapper();
 
+    @Override
+    public <T> T obtainData(String json, Class<T> tclass) {
+        try {
+            return mapper.readValue(json, tclass);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
