9e89797a3fba16af9b5adf3b03c157ceb6ab20d3
kai <kaisales.dev@proton.me>
2026-03-05 19:15:42 -0300

    Add SeasonData record class for season parameters

---
+++ src/main/java/br/com/alura/screenmatch/model/SeasonData.java
@@ -0,0 +1,9 @@
+package br.com.alura.screenmatch.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record SeasonData(Integer season, List<EpisodeData> episodes) {
+}
